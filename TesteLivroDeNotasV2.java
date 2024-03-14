8c1d36976007b2ee8d5bc41462fadffbb095a168
Lucas Kazuhiro <lucaslko429@gmail.com>
2024-03-14 16:19:54 -0300

    test(ap006): testa getters/setters e construtor

---
+++ TesteLivroDeNotasV2.java
@@ -0,0 +1,29 @@
+import javax.swing.JOptionPane;
+
+public class TesteLivroDeNotasV2{
+    public static void main(String[] args){
+
+        var livroDeNotas = new LivroDeNotas("ola");
+        var livroDeNotas2 = new LivroDeNotas("lucas");
+
+
+        String nomeDisciplina = JOptionPane.showInputDialog("Insira o nome da disciplina!");
+
+        livroDeNotas.setNomeDiscipliana(nomeDisciplina);
+        System.out.println(livroDeNotas.getNomeDisciplina());
+        livroDeNotas.exibirMensagem();
+
+
+        
+        System.out.println("\n\n\n");
+
+
+
+        nomeDisciplina = JOptionPane.showInputDialog("Insira o nome da disciplina!");
+
+        livroDeNotas2.setNomeDiscipliana(nomeDisciplina);
+        System.out.println(livroDeNotas.getNomeDisciplina());
+        livroDeNotas2.exibirMensagem();
+
+    }
+}
