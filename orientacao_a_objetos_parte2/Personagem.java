e308dcb6ce1c8c0dc45d3b11b36cd3e582061e21
Lucas Kazuhiro <lucaslko429@gmail.com>
2024-03-14 17:48:39 -0300

    feat(ap006-p2): implementa classe personagem
    - somente atributos e metodos
    - nao usa encapsulamento

--- orientacao_a_objetos_parte2/Personagem.java
+++ orientacao_a_objetos_parte2/Personagem.java
@@ -1,5 +1,20 @@
 package orientacao_a_objetos_parte2;
 
 public class Personagem {
-    
+    String nome;
+    int energia;
+    int fome;
+    int sono;
+
+    void cacar(){
+        System.out.printf("%s esta cacando...\n", nome);
+    }
+
+    void comer(){
+        System.out.printf("%s esta comendo...\n", nome);
+    }
+
+    void dormir(){
+        System.out.printf("%s esta dormindo", nome);
+    }
 }
