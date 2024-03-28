3feb52aa392a1fa7f244877b6b9a2efaa9d8e816
Lucas Kazuhiro <lucaslko429@gmail.com>
2024-03-28 18:07:01 -0300

    test(ap006): testa se um numero é potencia de 2

---
+++ colecoes/VetorDinamico.java
@@ -0,0 +1,26 @@
+public class VetorDinamico {
+    // Variáveis de instância (cada instância ou objeto tem a sua cópia)
+    private int qtde;
+    private int cap;
+    private int [] elementos;
+
+    // Variável de classe (todas as instâncias compartilham este mesmo valor)
+    public static final int CAP_MINIMA = 4;
+
+    VetorDinamico(){
+        cap = CAP_MINIMA;
+        qtde = 0;
+        elementos = new int[CAP_MINIMA];
+    }
+
+    VetorDinamico(int capMinima){
+        double aux = capMinima;
+        while(aux >= 2){
+            aux /= 2;
+        }
+        cap = aux == 1 && capMinima > 3 ? capMinima : CAP_MINIMA;
+    }
+
+
+
+}
