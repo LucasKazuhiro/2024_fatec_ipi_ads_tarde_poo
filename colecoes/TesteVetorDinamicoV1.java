3de17e2cc369c3a8cc3c2798abf85226ba8c77d5
Lucas Kazuhiro <lucaslko429@gmail.com>
2024-04-06 17:20:20 -0300

    test(ap006): insere um loop infinito para testar vetordinamico

--- colecoes/TesteVetorDinamicoV1.java
+++ colecoes/TesteVetorDinamicoV1.java
@@ -1,23 +1,21 @@
 import java.util.Random;
 
 public class TesteVetorDinamicoV1{
-  public static void main(String[] args) {
+  public static void main(String[] args)  throws Exception{
     var v = new VetorDinamico(8);
     var gerador = new Random();
 
-    int count=0;
-    while(count < 17){
+
+    System.out.printf("\n\n\n\n");
+
+
+    while(true){
       v.adicionar(gerador.nextInt(6) + 1);
-      count++;
+      System.out.println(v);
+      System.out.println("\n************************\n");
+      Thread.sleep(5000);
     }
 
-    System.out.printf("\n\n\n\n");
-
-    for(int valor : v.getElementos()){
-      System.out.printf("%d ", valor);
-    }
-
-    System.out.printf("\n\n\n\n");
-
+    
   }
 }
